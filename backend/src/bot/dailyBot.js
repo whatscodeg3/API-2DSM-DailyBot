275448356f088329ab286811238f652110a77122
MatheusAJesus <matheus_augustoja@yahoo.com.br>
2022-09-11 03:04:22 -0300

    Modificando o robo Foi modificado a busca de  automatizacao da pagina para link

--- backend/src/bot/dailyBot.js
+++ backend/src/bot/dailyBot.js
@@ -1,42 +1,54 @@
 //importação de bibliotecas
 const puppeteer = require('puppeteer');  //precisa ser instalada > npm install puppeteer
-const readlineSync = require('readline-sync');
+const readlineSync = require('readline-sync'); //precisa ser instalada > npm install readlin-sync
+const download = require('download-pdf'); 
 
 // coleta do nome a buscar
-console.log('Bem vindo ao Bot Buscador do Diário do Estado de São Paulo 🤖');
-const nomeAssociado = readlineSync.question('Informe um nome para procurar:'); //captura do nome
+const nomeAssociado = readlineSync.question('Informe um nome para procurar:');
+const nomeAssociadoMais = nomeAssociado.replace(/ /g,'+'); //insere simbolos + no lugar dos espaços
+const nomeAssociadoPorcento2 = nomeAssociadoMais.replace(/ '+' /g, '%2b'); //insere %2b no lugar dos +
 
-async function robo() { //inicio da funcão assincrona
-  const browser = await puppeteer.launch({ headless: false }); //inicia o navegador e o mantem aberto 
-  const page = await browser.newPage(); //abre nova guia e insere na varia page
-  
-  await page.goto('http://www.diariooficial.sp.gov.br/DO/BuscaDO2001_11_2.aspx#'); //adiciona o link em page e nevega
-  await page.type('#txtPalavrasChave', `"${nomeAssociado}"`);
 
-  const coletaDataHoje = await page.evaluate(() => {
+// função assincrona robo()
+async function robo() {
+  //abrindo navegador
+  const browser = await puppeteer.launch({ headless: true}); 
+  const page = await browser.newPage();
+
+  //coletando a data final que o site oferece para consulta
+  await page.goto('http://www.diariooficial.sp.gov.br/DO/BuscaDO2001_11_2.aspx#');
+  const dataDia = await page.evaluate(() => {
     return document.querySelector("#txtDataFim").value;
-  }); //coleta a data de hoje do campo data final
-    
-  await page.click('#content_content_content_chkGrupos_4'); //clica no grupo executivo
-  await page.click('#content_content_content_chkGrupos_1'); //clica no grupo cidade de são paulo
-  const inserirDataHoje = await page.$('#txtDataInicio'); //seleciona o campo data inicial
-  await inserirDataHoje.click({ clickCount: 3 }); //clica 3 vezes neste campo
-  await inserirDataHoje.type(coletaDataHoje); //insere a data coletada na variavel coletaDataHoje
-  await page.click('#content_content_content_btnBuscar'); //clica no botão buscar
+  });
+  const dataPonto = dataDia.replace(/\//g, '.')
+
+  //acessando link modificado com as informações para busca
+  await page.goto(`http://www.diariooficial.sp.gov.br/DO/BuscaDO2001Resultado_11_3.aspx?filtropalavraschave="${nomeAssociadoMais}"&f=xhitlist&xhitlist_vpc=first&xhitlist_x=Advanced&xhitlist_q=%5bfield+%27dc%3adatapubl%27%3a%3e%3d${dataPonto}%3c%3d${dataPonto}%5d("${nomeAssociadoPorcento2}")&filtrogrupos=Cidade+de+SP%2c+Executivo+&`); 
+
+  //coleta do link do resultado da busca 
+  //TODO criar o for para buscar todos os links que resultarem da busca.
+  const link = await page.evaluate(() => {
+  return document.querySelector("#form > div.container > div > div.col > div:nth-child(4) > div.card-body > p > a").href;
+  });
+
+  //modificando link para download
+  const pdf = link.replace('BuscaDO2001Documento_11_4.aspx', 'GatewayPDF.aspx') 
   
-  const botao = await page.waitForSelector('body > div:nth-child(5) > div > a.joyride-close-tip', { visible: true }); //aguarda modal aparecer e encontra botão fechar e insere na variavel botao
-  await botao.click(); //clica no botao fechar do modal
+  //relizando download
+  const options = {
+    directory: `./backend/src/bot/pdf/${nomeAssociado}`,//caminho onde será salvo
+    filename: `${nomeAssociado}.${dataPonto}.pdf` //nome do arquivo com nome do associado e data
+  }
+  download(pdf, options, function(err){
+    if(err) throw err
+    console.log('baixado')
+  })
 
-  
-  
-  const coletaLink = await page.evaluate(() => {
-    return document.querySelectorAll('a.bg-light.text-dark').href;
-  }); //coleta o primeiro link
+  //fechando navegador
+  await browser.close();
 
-  console.log(coletaLink) //mostra link no navegador
 
-//await browser.close();   //fechar navegador
+} // fim da função robo()
 
-}
-
-robo();  //executa o robô
+//chamando a função robo()
+robo();  
