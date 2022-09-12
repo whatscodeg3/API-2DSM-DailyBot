742c437bde744ab68ac32193b0c580742376b7e8
PHCorra <ph.corra@gmail.com>
2022-09-12 12:39:10 -0300

    feat: Criando a parte de repositorios para mexer no banco de forma independente das requisições

--- backend/src/controllers/AssociadoController.ts
+++ backend/src/controllers/AssociadoController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AssociadoModel } from "../database/models/associados";
+import { AssociadoRepository } from "../repositories/associadoRepository";
 
 interface IAssociadoController {
   create: (req: Request, res: Response) => Promise<Response>;
@@ -10,48 +11,50 @@ interface IAssociadoController {
 
 }
 
-class AssociadoController implements IAssociadoController {
+
+export class AssociadoController implements IAssociadoController {
+  private associadoRepository: AssociadoRepository
+  constructor(associadoRepository: AssociadoRepository) {
+    this.associadoRepository = associadoRepository;
+  }
+
   async create(req: Request, res: Response) {
-    const { nome, cpf, email, dataNascimento } = req.body;
-    const associado = await AssociadoModel.create({
-      nome,
-      cpf,
-      email,
-      dataNascimento,
-    });
+    const associadoRepository = new AssociadoRepository();
+    const associado = await associadoRepository.create(req.body);
     return res.status(201).json(associado);
   };
 
   async findAll(req: Request, res: Response) {
-    const associados = await AssociadoModel.findAndCountAll({});
-    return associados.rows.length > 0
-      ? res.status(200).json(associados)
-      : res.status(404).send("Nenhum associado encontrado");
+    try {
+      const associadoRepository = new AssociadoRepository();
+      const associados = await associadoRepository.findAll();
+      return associados.rows.length > 0
+        ? res.status(200).json(associados)
+        : res.status(404).send("Nenhum associado encontrado");
+    } catch (err) {
+      console.log(err);
+    }
   };
   async findOne(req: Request, res: Response) {
     const { id } = req.params;
-    const associado = await AssociadoModel.findOne({ where: { id } });
+    const associadoRepository = new AssociadoRepository();
+    const associado = await associadoRepository.findOne(id);
     return associado
       ? res.status(200).json(associado)
       : res.status(404).send("Associado Não encontrado")
   };
   async update(req: Request, res: Response) {
     const { id } = req.params;
-    const { nome, cpf, email, dataNascimento } = req.body;
-    await AssociadoModel.update({
-      nome,
-      cpf,
-      email,
-      dataNascimento,
-    }, { where: { id } });
+    const associadoRepository = new AssociadoRepository();
+    await associadoRepository.update(id, req.body);
     return res.status(200).send("Associado atualizado com sucesso");
   };
   async delete(req: Request, res: Response) {
     const { id } = req.params;
-    await AssociadoModel.destroy({ where: { id } })
+    const associadoRepository = new AssociadoRepository();
+    await associadoRepository.delete(id);
     return res.status(204).send("Associado deletado com sucesso");
   };
-
 }
 
-export default new AssociadoController();
+export default new AssociadoController(new AssociadoRepository);
