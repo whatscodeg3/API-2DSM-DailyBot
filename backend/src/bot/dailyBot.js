2963efa21a8e078bd4e1cb193fc923c0da686064
MatheusAJesus <matheus_augustoja@yahoo.com.br>
2022-09-15 12:21:56 -0300

    Arrumando o código fiz um teste e deixei comentado no final para passar uma lista de nomes e acessar os links de cada nome pesquisado

--- backend/src/bot/dailyBot.js
+++ backend/src/bot/dailyBot.js
@@ -1,19 +1,16 @@
-//importação de bibliotecas
-const puppeteer = require('puppeteer');  //precisa ser instalada > npm install puppeteer
-const readlineSync = require('readline-sync'); //precisa ser instalada > npm install readlin-sync
+const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
 const download = require('download-pdf'); 
 const cheerio = require('cheerio');
 
-// coleta do nome a buscar
-const nomeAssociado = readlineSync.question('Informe um nome para procurar:');
-const nomeAssociadoMais = nomeAssociado.replace(/ /g,'+'); //insere simbolos + no lugar dos espaços
-const nomeAssociadoPorcento2 = nomeAssociadoMais.replace(/ '+' /g, '%2b'); //insere %2b no lugar dos +
-
-
-// função assincrona robo()
-async function robo() {
+/*------- inicio da função robo --------*/
+async function robo(nomeAssociado){
+  
+  const nomeAssociadoMais = nomeAssociado.replace(/ /g,'+'); //insere simbolos + no lugar dos espaços
+  const nomeAssociadoPorcento2 = nomeAssociadoMais.replace(/ '+' /g, '%2b'); //insere %2b no lugar dos +
+ 
   //abrindo navegador
-  const browser = await puppeteer.launch({ headless: false}); 
+  const browser = await puppeteer.launch({ headless: true}); 
   const page = await browser.newPage();
 
   //coletando a data final que o site oferece para consulta
@@ -27,6 +24,7 @@ async function robo() {
   const linkBusca =`http://www.diariooficial.sp.gov.br/DO/BuscaDO2001Resultado_11_3.aspx?filtropalavraschave=%22${nomeAssociadoMais}%22&f=xhitlist&xhitlist_vpc=first&xhitlist_x=Advanced&xhitlist_q=%5bfield+%27dc%3adatapubl%27%3a%3e%3d${dataPonto}%3c%3d${dataPonto}%5d(${nomeAssociadoPorcento2})&filtrogrupos=Cidade+de+SP%2c+Executivo+&xhitlist_mh=9999&filtrodatafimsalvar=${dataSeparadaInvertida.join('')}&filtroperiodo=${dataSeparadaInvertida[2]}%2f${dataSeparadaInvertida[1]}%2f${dataSeparadaInvertida[0]}+a+${dataSeparadaInvertida[2]}%2f${dataSeparadaInvertida[1]}%2f${dataSeparadaInvertida[0]}&filtrodatainiciosalvar=${dataSeparadaInvertida.join('')}&filtrogrupossalvar=Cidade+de+SP%2c+Executivo+&xhitlist_hc=%5bXML%5d%5bKwic%2c3%5d&xhitlist_vps=15&filtrotodosgrupos=False&xhitlist_d=Cidade+de+SP%2c+Executivo+&filtrotipopalavraschavesalvar=UP&xhitlist_s=&xhitlist_sel=title%3bField%3adc%3atamanho%3bField%3adc%3adatapubl%3bField%3adc%3acaderno%3bitem-bookmark%3bhit-context&xhitlist_xsl=xhitlist.xsl`
 
   //acessando link modificado com as informações para busca
+  const linksPdfs = [];
   await page.goto(linkBusca); 
   const linkResultado = page.url();
   switch(linkResultado.includes('ResultadoNegativo')){
@@ -37,7 +35,7 @@ async function robo() {
       const $ = cheerio.load(html);
 
       const seletor = '#form > div.container > div > div.col > div.card.shadow-sm.resultadoBuscaItem > div.card-body > p > a.bg-light.text-dark';
-      let linksPdfs = [];
+  
           $(seletor).each(function () {
               let link = this.attribs.href;
               link =  link.replace(/ /g, '%20')
@@ -45,8 +43,6 @@ async function robo() {
               const pdf = link.replace('BuscaDO2001Documento_11_4.aspx', 'GatewayPDF.aspx')
               linksPdfs.push(pdf)              
           });
-
-          console.log(linksPdfs)
       
       //relizando download
       var pagina = 1;
@@ -57,19 +53,31 @@ async function robo() {
       }      
         download(pdf, options, function(err){
         if(err) throw err
-        console.log('baixado')
-      })
+        console.log('baixado');
+      });
         pagina++
       }
-
-
-
    }
 
   //fechando navegador
-  // await browser.close();
+  await browser.close();
+  return linksPdfs;
 
-} // fim da função robo()
+} 
 
-//chamando a função robo()
-robo();
+/*------- fim da função robo()--------*/
+
+//teste com lista
+// const assoc = ['ensino', 'rua', 'joao']
+// let linksPdfs = []
+// for( const nomeAssociado of assoc){
+//   robo(nomeAssociado).then(linksPorUsuario => {
+//     // got value here
+//     linksPdfs.push(linksPorUsuario);
+// }).catch(e => {
+//     // error
+//     console.log(e);
+// });
+// }
+
+// setTimeout(() => {console.log(linksPdfs)}, 30000)
--- backend/src/bot/testes.js
+++
@@ -1,9 +0,0 @@
-// dataDia = '02/02/2022'
-// const dataPonto = dataDia.replace(/\//g, '.')
-// console.log(dataPonto) GatewayPDF.aspx
-
-var link  = 'http://www.diariooficial.sp.gov.br/DO/BuscaDO2001Documento_11_4.aspx?link=%2f2022%2fexecutivo+secao+i%2fsetembro%2f10%2fpag_0050_1168983ef2310f3c984022a979550724.pdf&pagina=50&data=10/09/2022&caderno=Executivo%20I&paginaordenacao=100050'
-
-var link2 = link.replace('BuscaDO2001Documento_11_4.aspx', 'GatewayPDF.aspx')
-
-console.log(link2)
