986ce9c6a5f07b8a65f86b3f25de37a9596dc806
LucasMedici <lucasrpmedici@gmail.com>
2022-09-22 22:05:37 -0300

    inserindo leitura de varios PDFs

--- backend/src/robo_leitor_pdf/parser.py
+++ backend/src/robo_leitor_pdf/parser.py
@@ -1,17 +1,32 @@
+import json
 import re
+from os import listdir
 from PyPDF2 import PdfFileReader
 
-pdf_para_leitura = PdfFileReader('PDFS/pg_0085.pdf')
-page_object = pdf_para_leitura.getPage(0)
+nomes_pdfs=[]
+def listandoFiles(diretorio):
+    for i in listdir(diretorio):
+        nomes_pdfs.append(i)
+        
+listandoFiles('PDFs/')
 
-texto_do_pdf = page_object.extract_text()
+dicio={}
+lista=[]
+for nome in nomes_pdfs:
+    pdf_para_leitura = PdfFileReader('PDFs/'+nome)
+    page_object = pdf_para_leitura.getPage(0)
+    texto_do_pdf = page_object.extract_text()
+    blocos_de_texto = texto_do_pdf.split('\n')
+    for i in blocos_de_texto:
+        nome=nome.replace('.pdf','')
+        if re.search(nome, i, re.IGNORECASE):
+            bloco_relacionado_associado = i
+            # print(bloco_relacionado_associado)
+            dicio['nome']=nome
+            dicio['trecho']=bloco_relacionado_associado
+            lista.append(dicio)
+           
 
-blocos_de_texto = texto_do_pdf.split('\n')
-
-
-for i in blocos_de_texto:
-    if re.search('daniel FELICIANO', i, re.IGNORECASE):
-        bloco_relacionado_associado = i
-
-
-print(bloco_relacionado_associado)
+json_object = json.dumps(lista) 
+with open("lista.json", "w") as outfile: 
+    outfile.write(json_object) 
