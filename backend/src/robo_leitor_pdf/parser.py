b4170d50ccdb28d0b91968c53a70da715fdcff53
LucasMedici <lucasrpmedici@gmail.com>
2022-09-26 16:40:49 -0300

    criando um json com os pdfs lidos

--- backend/src/robo_leitor_pdf/parser.py
+++ backend/src/robo_leitor_pdf/parser.py
@@ -10,23 +10,23 @@ def listandoFiles(diretorio):
         
 listandoFiles('PDFs/')
 
-dicio={}
+
 lista=[]
 for nome in nomes_pdfs:
     pdf_para_leitura = PdfFileReader('PDFs/'+nome)
     page_object = pdf_para_leitura.getPage(0)
     texto_do_pdf = page_object.extract_text()
     blocos_de_texto = texto_do_pdf.split('\n')
+    dicio={}
     for i in blocos_de_texto:
         nome=nome.replace('.pdf','')
         if re.search(nome, i, re.IGNORECASE):
             bloco_relacionado_associado = i
-            # print(bloco_relacionado_associado)
             dicio['nome']=nome
             dicio['trecho']=bloco_relacionado_associado
             lista.append(dicio)
-           
-
-json_object = json.dumps(lista) 
-with open("lista.json", "w") as outfile: 
-    outfile.write(json_object) 
+            
+            
+             
+with open("lista.json", "w", encoding="utf-8") as jsonfile: 
+    json.dump(lista, jsonfile, ensure_ascii=False) 
