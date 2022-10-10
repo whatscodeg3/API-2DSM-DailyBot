00ce61b985812eaa4e095638f35d8ea11254ad4e
MatheusAJesus <matheus_augustoja@yahoo.com.br>
2022-10-09 23:31:59 -0300

    modificando dados da conexao bd no leitor.py

--- backend/src/robo_leitor_pdf/leitor.py
+++ backend/src/robo_leitor_pdf/leitor.py
@@ -6,7 +6,7 @@ from PyPDF2 import PdfFileReader
 import mysql.connector
 
 def leitor():
-    db_connection = mysql.connector.connect(host="localhost", user="MatheusAJesus", passwd="@Fatec2022", database="apimidall")
+    db_connection = mysql.connector.connect(host="localhost", user="root", passwd="root", database="apimidall")
     cursor = db_connection.cursor(buffered=True)
 
 
