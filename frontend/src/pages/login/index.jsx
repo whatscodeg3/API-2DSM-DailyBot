fb53aab21fac71081a476cc6392f8d0a2c6f743a
fecosta290 <100285183+fecosta290@users.noreply.github.com>
2022-11-05 21:01:11 -0300

    arrumando tela de login

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -7,8 +7,6 @@ import { DivLogin, DivImage, DivGeral } from './styles'
 import { GlobalStyle } from './globalStyles'
 
 
-
-
 function Login() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = async data => {
@@ -22,24 +20,24 @@ function Login() {
     console.log(userDatabaseData);
   }
 
-    ;
   return (
-
-
-    <DivGeral onSubmit={handleSubmit(onSubmit)}>
-      <GlobalStyle />
-      <DivImage>
-        <img src={robo} alt="robo" />
-        <h1 style={{ color: 'white' }}>Olá, eu sou Dailybot...</h1>
-      </DivImage>
-      <DivLogin>
-        <label htmlFor="email" ><b style={{ color: 'white' }}>E-mail</b></label>
-        <input type="text" name="email" placeholder="Digite seu email" {...register("email", { required: true })} />
-        <label htmlFor="password"><b style={{ color: 'white' }}>Senha</b></label>
-        <input type="password" name="password" placeholder="Digite sua senha" {...register("senha", { required: true })} />
-        <button type="submit" id="botaoLogin">Login</button>
-      </DivLogin>
-    </DivGeral>
+      <>
+      <DivGeral onSubmit={handleSubmit(onSubmit)}>
+        <GlobalStyle />
+        <DivImage>
+          <img src={robo} alt="robo" />
+          <h1>Olá, eu sou Dailybot...</h1>
+        </DivImage>
+        <DivLogin>
+          <h1>Login</h1>
+          <label htmlFor="email" ><b>E-mail:</b></label>
+          <input type="text" name="email" placeholder="Digite seu email" {...register("email", { required: true })} />
+          <label htmlFor="password"><b>Senha:</b></label>
+          <input type="password" name="password" placeholder="Digite sua senha" {...register("senha", { required: true })} />
+          <button type="submit" id="botaoLogin">Entrar</button>
+        </DivLogin>
+      </DivGeral>
+      </>
   )
 }
 
